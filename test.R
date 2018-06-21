01652f6a84257fba7f48b2c84a170936a69ad544
Unknown <marc.kukawka@googlemail.com>
2018-06-21 21:52:48 +0200

    initial r script

---
+++ test.R
@@ -0,0 +1,8 @@
+# Load the package required to read JSON files.
+library("rjson")
+
+# Give the input file name to the function.
+result <- fromJSON(file = "")
+
+# Print the result.
+json_data <- fromJSON()
